9801492710a56e3681d66381f2563dbeb020b20b
larrykirschner <larrykirschner@gmail.com>
2017-09-05 17:10:39 -0700

    0.0.2

---
+++ src/PanelInterfaces/PanelBase.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using BeatThat.UI;
+using BeatThat.Anim;
+
+namespace BeatThat.UI.Comp
+{
+	public abstract class PanelBase : MonoBehaviour, Panel
+	{
+		public Transition PrepareTransition(PanelTransition t, OnTransitionFrameDelegate onFrameDel = null)
+		{
+			return this._panel.PrepareTransition(t, onFrameDel);
+		}
+
+		public void StartTransition(PanelTransition t)
+		{
+			this._panel.StartTransition(t);
+		}
+
+		public void BringInImmediate()
+		{
+			this._panel.BringInImmediate();
+		}
+
+		public void DismissImmediate()
+		{
+			this._panel.DismissImmediate();
+		}
+		
+		abstract protected Panel _panel { get; }
+		
+	}
+}
